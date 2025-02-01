95ec986bbce81fa0b7a2251b8b5993ceb476dfec
Sahil Suman <sahilsuman1202@gmail.com>
2025-02-01 13:35:26 +0530

    tryying

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,9 +2,16 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps as NextThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: "class" | "data-theme" | Array<"class" | "data-theme">
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   const [mounted, setMounted] = React.useState(false)
 
   React.useEffect(() => {
